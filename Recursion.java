f262a538bf31bcded031ef4d3e2a44d570a6a303
aditihaiman <ahaiman00@stuy.edu>
2019-01-31 12:07:23 -0500

    testing sqrt

--- Recursion.java
+++ Recursion.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 public class Recursion {
 
+  public static void main(String[] args) {
+    // Sqrt Testing //
+    System.out.println("16: " + sqrt(16, .001) + "; " + Math.sqrt(16));
+    System.out.println("100: " + sqrt(100, .001) + "; " + Math.sqrt(100));
+    System.out.println("1: " + sqrt(1, .001) + "; " + Math.sqrt(1));
+    System.out.println(".5: " + sqrt(.5, .001) + "; " + Math.sqrt(.5));
+
+
+
+  }
+
+
   public static double sqrt(double n, double tolerance){
     if (n==0) return 0;
     return helperSquareRoot(n, 1, tolerance);
