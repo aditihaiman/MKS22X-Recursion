fa25a73e8a524e2ee671a5ef7546e07f972d975f
aditihaiman <ahaiman00@stuy.edu>
2019-01-31 12:03:39 -0500

    updated with new parameters

--- Recursion.java
+++ Recursion.java
@@ -4,10 +4,10 @@ public class Recursion {
 
   public static double sqrt(double n, double tolerance){
     if (n==0) return 0;
-    return helperSquareRoot(n, 1, .001);
+    return helperSquareRoot(n, 1, tolerance);
   }
 
-  public static double helperSquareRoot(int n, double guess, double percent) {
+  private static double helperSquareRoot(double n, double guess, double percent) {
     if (Math.abs((n-guess*guess) / n * 100) <= percent) return guess;
     guess = (n / guess + guess) / 2;
     return helperSquareRoot(n, guess, percent);
