11bfb670d110c1a1edea3234b4a758ebcc78e823
aditihaiman <ahaiman00@stuy.edu>
2019-01-31 12:02:47 -0500

    transferring work for sqrt

--- Recursion.java
+++ Recursion.java
@@ -3,8 +3,14 @@ import java.util.*;
 public class Recursion {
 
   public static double sqrt(double n, double tolerance){
-
+    if (n==0) return 0;
+    return helperSquareRoot(n, 1, .001);
   }
 
+  public static double helperSquareRoot(int n, double guess, double percent) {
+    if (Math.abs((n-guess*guess) / n * 100) <= percent) return guess;
+    guess = (n / guess + guess) / 2;
+    return helperSquareRoot(n, guess, percent);
+  }
 
 }
